b82d02290067c7c1a69520d7d815a2b7ddefa283
iamzaidsarwar_ <82316311+sarwar0001@users.noreply.github.com>
2022-06-18 11:35:49 +0530

    Add files via upload
    Print all the numbers till given number using recursion

---
+++ counting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void print(int num)
+{
+    if(num==0)
+    {
+        return;
+    }
+    
+    print(num-1);
+    cout<<num<<" ";   
+}
+
+int main()
+{
+    int num;
+    cin>>num;
+    print(num);
+    return 0;
+}
